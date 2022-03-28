5e19ab344336a51a8e7a634098dd3279db100e68
Julian Carrillo <78111031+Jacarrilloc@users.noreply.github.com>
2022-03-27 20:20:54 -0500

    Contactos Actulaizado
    Falta corregir la solicitud del permiso

--- Taller 2 CompuMovil/app/src/main/java/com/example/taller2/ContactosActividad.java
+++ Taller 2 CompuMovil/app/src/main/java/com/example/taller2/ContactosActividad.java
@@ -1,14 +1,68 @@
 package com.example.taller2;
 
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class ContactosActividad extends AppCompatActivity {
+
+    String[] mProjection;
+    Cursor mCursor;
+    ContactsAdapter mContactsAdapter;
+    ListView mlista;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contactos_actividad);
+
+        mlista = findViewById(R.id.listaContactos);
+
+        mProjection = new String[]{
+                ContactsContract.Profile._ID,
+                ContactsContract.Profile.DISPLAY_NAME_PRIMARY
+        };
+        mCursor = getContentResolver().query(ContactsContract.Contacts.CONTENT_URI,
+                mProjection, null, null, null);
+
+        mContactsAdapter = new ContactsAdapter(this, null, 0);
+        mlista.setAdapter(mContactsAdapter);
+        getSinglePermission.launch(Manifest.permission.READ_CONTACTS);
+
     }
+
+    ActivityResultLauncher<String> getSinglePermission = registerForActivityResult(
+            new ActivityResultContracts.RequestPermission(),
+            new ActivityResultCallback<Boolean>() {
+                @Override
+                public void onActivityResult(Boolean result) {
+                    if(result == true){
+                        initView();
+                    }else{
+
+                    }
+                }
+            });
+
+    public void initView(){
+        if(ContextCompat.checkSelfPermission(this,
+                Manifest.permission.READ_CONTACTS)==
+                PackageManager.PERMISSION_GRANTED){
+            mCursor=getContentResolver().query(
+                    ContactsContract.Contacts.CONTENT_URI,
+                    mProjection, null, null, null);
+            mContactsAdapter.changeCursor(mCursor);
+        }
+    }
+
 }
